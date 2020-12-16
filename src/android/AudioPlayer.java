dde5de9b60538c94b3bfd4f2465d64805a195b03
jakob-p <jakob.pfab@gmail.com>
2020-12-16 15:07:52 +0100

    Update AudioPlayer.java
    + avoid requesting external storage

--- src/android/AudioPlayer.java
+++ src/android/AudioPlayer.java
@@ -108,11 +108,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
 
     private String generateTempFile() {
       String tempFileName = null;
-      if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-          tempFileName = Environment.getExternalStorageDirectory().getAbsolutePath() + "/tmprecording-" + System.currentTimeMillis() + ".3gp";
-      } else {
-          tempFileName = "/data/data/" + handler.cordova.getActivity().getPackageName() + "/cache/tmprecording-" + System.currentTimeMillis() + ".3gp";
-      }
+      tempFileName = "/data/data/" + handler.cordova.getActivity().getPackageName() + "/cache/tmprecording-" + System.currentTimeMillis() + ".3gp";
       return tempFileName;
     }
 
@@ -195,11 +191,7 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
         /* this is a hack to save the file as the specified name */
 
         if (!file.startsWith("/")) {
-            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                file = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + file;
-            } else {
                 file = "/data/data/" + handler.cordova.getActivity().getPackageName() + "/cache/" + file;
-            }
         }
 
         int size = this.tempFiles.size();
@@ -706,9 +698,6 @@ public class AudioPlayer implements OnCompletionListener, OnPreparedListener, On
                     FileInputStream fileInputStream = new FileInputStream(file);
                     this.player.setDataSource(fileInputStream.getFD());
                     fileInputStream.close();
-                }
-                else {
-                    this.player.setDataSource(Environment.getExternalStorageDirectory().getPath() + "/" + file);
                 }
             }
                 this.setState(STATE.MEDIA_STARTING);
